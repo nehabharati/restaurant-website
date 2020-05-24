5297be0b67a8e1e700a2eb1b5510f6b89669ba6a
nehabharati <nsbharati2008@gmail.com>
2020-05-24 22:54:26 +0530

    website done

--- src/components/Register.js
+++ src/components/Register.js
@@ -5,8 +5,23 @@ function Register() {
 
     function handleSubmit(e){
         e.preventDefault()
-        document.getElementById("form").reset()
-        window.location = "/login"
+       
+        const form = document.getElementById('form')
+        const mail = document.getElementById('email') 
+        const small = form.querySelector('small')
+        console.log(mail.value)
+        let reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+        if(reg.test(mail.value) === false) {
+            small.innerText = "Please enter valid email"
+            small.style.display = "block"
+        } 
+        else {
+            small.innerText = "Email is correct"
+            small.style.display = "none"
+            document.getElementById("form").reset()
+            window.location = "/login"
+        } 
     }
     return(
         <div className="h-screen bg-blue-200">
@@ -15,14 +30,14 @@ function Register() {
                     <div className="pl-8 pr-8 pt-8"> 
                         <label className="" id = 'email'>Email</label>
                         <input type = 'email' placeholder = 'Email' id = 'email' className=" my-2 border border-blue-700 w-full rounded p-1"/>
+                        <small className="hidden -mt-2 text-red-700">Error message</small>
                         <label className="mb-1">Password</label>
                         <input type = 'password' placeholder='Password' id = 'password' className="my-2 border border-blue-700 w-full rounded p-1"/>
                         <div id="legend" className="z-10 w-full">
-                            <button id="reg" className="btn border-0 pt-2 pb-6 m-auto w-full text-white bg-transparent mt-16 focus:outline-none" type='submit'><span>REGISTER</span></button>
+                            <button id="reg" className="btn pt-2 pb-6 m-auto w-full text-white bg-transparent mt-16 focus:outline-none" type='submit'><span>REGISTER</span></button>
                         </div>
                     </div>
-                    <small className="hidden">Error message</small>
-                </form>
+                 </form>
             </div>
         </div>
     )
